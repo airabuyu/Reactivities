eca8c0aaccc6242ac289fd9f4f0f494ae0b9e45a
jatericx <jatericx@gmail.com>
2023-11-08 23:27:41 +0700

    Add Form Component and Button Methods

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,20 +3,34 @@ import { Grid, List } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetail from '../details/ActivityDetail';
+import ActivityForm from '../form/ActivityForm';
 
 interface Props {
     activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
 }
 
-export default function ActivityDashboard({activities}: Props) {
+export default function ActivityDashboard({activities, selectedActivity
+        ,selectActivity, cancelSelectActivity, editMode, openForm, closeForm}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities}/>
+                <ActivityList activities={activities} selectActivity={selectActivity}/>
             </Grid.Column>
             <Grid.Column width='6'>
-                {activities[0] &&
-                <ActivityDetail activity={activities[0]} />}
+                {selectedActivity && !editMode &&
+                <ActivityDetail 
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity} 
+                    openForm={openForm}
+                />}
+                {editMode &&
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
             </Grid.Column>
         </Grid>
     )
